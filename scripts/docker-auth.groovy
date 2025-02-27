d0e1b17ad8cd3fa5b1c18b1fc8a08814238d7c5a
Sathya <sathyapathi@gmail.comm>
2025-02-26 20:12:03 -0500

    updated jenkins files

--- scripts/docker-auth.groovy
+++ scripts/docker-auth.groovy
@@ -1,9 +1,16 @@
-def dockerLogin(credentialsId) {
-    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+// scripts/docker-auth.groovy
+
+// Define DockerHub credentials ID in a separate variable for easy updates
+def DOCKER_HUB_CREDENTIALS_ID = "docker-hub-credentials"
+
+// Function to log in to DockerHub using Jenkins credentials
+def dockerLogin() {
+    withCredentials([usernamePassword(credentialsId: DOCKER_HUB_CREDENTIALS_ID, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin
         """
     }
 }
 
+// Expose the variable in case other scripts need to use it
 return this
